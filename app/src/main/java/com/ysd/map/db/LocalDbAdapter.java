16dc1d9a05c28965a183c636aa76549ef5e51e0a
yeshida <15980598149@139.com>
2016-01-27 23:24:23 +0800

    添加数据库类TrackDbAdapter

---
+++ app/src/main/java/com/ysd/map/db/LocalDbAdapter.java
@@ -0,0 +1,7 @@
+package com.ysd.map.db;
+
+/**
+ * Created by Administrator on 2016/1/27.
+ */
+public class LocalDbAdapter extends DbAdapter {
+}
--- app/src/main/java/com/ysd/map/db/TrackDbAdaper.java
+++ app/src/main/java/com/ysd/map/db/TrackDbAdaper.java
@@ -1,10 +1,13 @@
 package com.ysd.map.db;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import java.sql.SQLException;
+import java.util.Calendar;
 
 /**
  * Created by Administrator on 2016/1/26.
@@ -19,6 +22,7 @@ public class TrackDbAdaper extends DbAdapter {
     private static final String DIST = "distance";
     private static final String TRACKEDTIME = "tracked_time";
     private static final String LOCATE_COUNT = "locats_count";
+    private static final String CREATED = "created_at";
     private static final String UPDATED = "update_at";
     private static final String AVGSPEED = "avg_speed";
     private static final String MAXSPEED = "max_speed";
@@ -37,4 +41,39 @@ public class TrackDbAdaper extends DbAdapter {
     public void close(){
         mDbHelper.close();
     }
+    public Cursor getTrack(long rowId) throws SQLException {
+        Cursor mCursor = mDb.query(true, TABLE_NAME, new String[]{KEY_ROWID,NAME,DESC,CREATED}, KEY_ROWID + "=" + rowId, null, null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+    //增加
+    public long createTrack(String name, String desc) {
+        Log.d(TAG, "createTrack");
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(NAME,name);
+        initialValues.put(DESC,desc);
+        Calendar calendar = Calendar.getInstance();
+        String created = calendar.get(Calendar.YEAR) + "-" + calendar.get(Calendar.MONTH) + "-" + calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE) + ":" + calendar.get(Calendar.SECOND);
+        initialValues.put(CREATED,created);
+        initialValues.put(UPDATED,created);
+        return mDb.insert(TABLE_NAME, null, initialValues);
+    }
+    //删除
+    public boolean deleteTrack(long rowId) {
+        return mDb.delete(TABLE_NAME, KEY_ROWID + "=" + rowId, null) > 0;
+    }
+
+    //修改
+    public boolean updateTrack(long rowId, String name, String desc) {
+        ContentValues args = new ContentValues();
+        args.put(NAME,name);
+        args.put(DESC, desc);
+        Calendar calendar = Calendar.getInstance();
+        String updated = calendar.get(Calendar.YEAR) + "-" + calendar.get(Calendar.MONTH) + "-" + calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE) + ":" + calendar.get(Calendar.SECOND);
+        args.put(UPDATED,updated);
+        return mDb.update(TABLE_NAME, args, KEY_ROWID + "=" + rowId, null) > 0;
+    }
 }
+
