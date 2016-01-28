9440c523d85a13641f0eb99e2b84c5c391634856
yeshida <15980598149@139.com>
2016-01-28 23:11:08 +0800

    申请map key

--- app/src/main/java/com/ysd/map/activity/ShowTrack.java
+++ app/src/main/java/com/ysd/map/activity/ShowTrack.java
@@ -1,7 +1,13 @@
 package com.ysd.map.activity;
 
+import com.google.android.maps.MapActivity;
 /**
  * Created by Administrator on 2016/1/25.
  */
-public class ShowTrack {
+public class ShowTrack extends MapActivity{
+//定义菜单需要的常量
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
 }
